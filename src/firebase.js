b543eedf45d6ce23afe91b56ba2655d3d28ee239
Parth00 <dubalparth@gmail.com>
2020-09-14 18:08:18 -0400

    bugs fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "<API_KEY",
-    authDomain: "<AUTH_DOMAIN>",
-    databaseURL: "<DATABASE_URL>",
-    projectId: "<PROJECT_ID>",
-    storageBucket: "<STORAGE_BUCKET>",
-    messagingSenderId: "<MESSAGING_SENDER_ID>",
-    appId: "<APP_ID>"
+    apiKey: "AIzaSyBDMiUaFfPB4y8q9DF4XYZYXsh3dfg6c-E",
+    authDomain: "clone-c18a0.firebaseapp.com",
+    databaseURL: "https://clone-c18a0.firebaseio.com",
+    projectId: "clone-c18a0",
+    storageBucket: "clone-c18a0.appspot.com",
+    messagingSenderId: "1054108466827",
+    appId: "1:1054108466827:web:4cd47700ccbe192eee1e8e"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
